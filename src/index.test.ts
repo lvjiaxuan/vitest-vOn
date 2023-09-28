ff043206ad1a8eab3cbcac2f64744d7f43a0a6a8
lvjiaxuan <11309921+lvjiaxuan@users.noreply.github.com>
2023-09-28 07:11:59 +0000

    feat: new reg works

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,58 +1,54 @@
 // https://github.com/vuejs/core/pull/6769/files#diff-da74645a7be0341a07b5bb25ba48d8046f26553c7b3aa89a1b2b28a5d8c3440c
 import { expect, test } from 'vitest'
 
-const fnExpRE =
-  /^\s*([\w$_]+|(async\s*)?\([^)]*?\))\s*(:[^=]+)?=>|^\s*(async\s+)?function(?:\s+[\w$]+)?\s*\(/
+// old
+// const fnExpRE = /^\s*([\w$_]+|(async\s*)?\([^)]*?\))\s*(:[^=]+)?=>|^\s*(async\s+)?function(?:\s+[\w$]+)?\s*\(/
+const fnExpRE = /(^[\s\(]*((async\s*)?([\w$_]+|\([^)]*?\))\s*(:[^=]+)?=>|(async\s+)?function(?:\s+[\w$_]*)\s*\([^)]*?\)\s*(:[^=]+)?{))[\s\S]+\)*/
 
-const trimFnExpParens = (expression: string) =>
-  // from ai
-  '() => {}'
-
-
-test('should handle inline arrow function with parentheses', () => {
+test('should handle inline arrow function wrapped in parentheses', () => {
   const expression = '(foo => bar = foo)'
-  const expressionWithTS = '((foo: any) => bar = foo)'
+  const expressionWithTS = '((foo: any): any => bar = foo)'
 
-  expect(fnExpRE.test(trimFnExpParens(expression))).toBeTruthy()
-  expect(fnExpRE.test(trimFnExpParens(expressionWithTS))).toBeTruthy()
+  expect(fnExpRE.test(expression)).toBeTruthy()
+  expect(fnExpRE.test(expressionWithTS)).toBeTruthy()
 })
 
-test.skip('should handle inline function expression with parentheses', () => {
+test('should handle inline function expression wrapped in parentheses', () => {
   const expression = '(function (foo) { bar = foo })'
-  const expressionWithTS = '(function (foo: any) { bar = foo })'
+  const expressionWithTS = '(function (foo: any): any { bar = foo })'
 
-  expect(fnExpRE.test(trimFnExpParens(expression))).toBeTruthy()
-  expect(fnExpRE.test(trimFnExpParens(expressionWithTS))).toBeTruthy()
+  expect(fnExpRE.test(expression)).toBeTruthy()
+  expect(fnExpRE.test(expressionWithTS)).toBeTruthy()
 })
 
-test.skip('should handle inline async arrow function', () => {
-  const expression = 'async foo => { await fetch(foo); bar = foo }}'
-  const expressionWithTS = 'async (foo: any) => { await fetch(foo); bar = foo }}'
+test('should handle inline async arrow function', () => {
+  const expression = 'async foo => { await fetch(foo); bar = foo; }'
+  const expressionWithTS = 'async (foo: any): Promise<any> => { await fetch(foo); bar = foo }}'
 
-  expect(fnExpRE.test(trimFnExpParens(expression))).toBeTruthy()
-  expect(fnExpRE.test(trimFnExpParens(expressionWithTS))).toBeTruthy()
+  expect(fnExpRE.test(expression)).toBeTruthy()
+  expect(fnExpRE.test(expressionWithTS)).toBeTruthy()
 })
 
-test.skip('should handle inline async function expression', () => {
-  const expression = 'async function (foo) { await fetch(foo); bar = foo }}'
-  const expressionWithTS = 'async function (foo: any) { await fetch(foo); bar = foo }}'
+test('should handle inline async function expression', () => {
+  const expression = 'async function (foo) { await fetch(foo); bar = foo }}' // old reg works
+  const expressionWithTS = 'async function (foo: any): Promise<any> { await fetch(foo); bar = foo }}'
 
-  expect(fnExpRE.test(trimFnExpParens(expression))).toBeTruthy()
-  expect(fnExpRE.test(trimFnExpParens(expressionWithTS))).toBeTruthy()
+  expect(fnExpRE.test(expression)).toBeTruthy()
+  expect(fnExpRE.test(expressionWithTS)).toBeTruthy()
 })
 
-test.skip('should handle inline async arrow function with parentheses', () => {
+test('should handle inline async arrow function wrapped in parentheses', () => {
   const expression = '(async foo => { await fetch(foo); bar = foo }})'
-  const expressionWithTS = '(async (foo: any) => { await fetch(foo); bar = foo }})'
+  const expressionWithTS = '(async (foo: any): Promise<any> => { await fetch(foo); bar = foo }})'
 
-  expect(fnExpRE.test(trimFnExpParens(expression))).toBeTruthy()
-  expect(fnExpRE.test(trimFnExpParens(expressionWithTS))).toBeTruthy()
+  expect(fnExpRE.test(expression)).toBeTruthy()
+  expect(fnExpRE.test(expressionWithTS)).toBeTruthy()
 })
 
-test.skip('should handle inline async function expression with parentheses', () => {
-  const expression = '(async function (foo) { await fetch(foo); bar = foo }})'
-  const expressionWithTS = '(async function (foo: any) { await fetch(foo); bar = foo }})'
+test('should handle inline async function expression wrapped in parentheses', () => {
+  const expression = '(async function (foo) { await fetch(foo); bar = foo })'
+  const expressionWithTS = '(async function (foo: any): Promise<any> { await fetch(foo); bar = foo }})'
 
-  expect(fnExpRE.test(trimFnExpParens(expression))).toBeTruthy()
-  expect(fnExpRE.test(trimFnExpParens(expressionWithTS))).toBeTruthy()
+  expect(fnExpRE.test(expression)).toBeTruthy()
+  expect(fnExpRE.test(expressionWithTS)).toBeTruthy()
 })
